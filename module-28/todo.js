a0afe2b0e5130d2ac07a38ce682fc24b6de1c0b4
PromelHridoy <promel.hridoy12@gmail.com>
2026-03-03 12:30:12 +0600

    More explore API

---
+++ module-28/todo.js
@@ -0,0 +1,28 @@
+const loadTodo = () => {
+    const url ='https://jsonplaceholder.typicode.com/todos';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayTodo(data))
+}
+
+const displayTodo = (todos => {
+    // console.log(todos);
+    const todoContainer = document.getElementById("todo-container")
+    todoContainer.innerHTML ='';
+    todos.forEach(todo => {
+        console.log(todo);
+
+        const todoCard = document.createElement("div");
+        todoCard.innerHTML =`
+        <div class="todo-card">
+            <p>${todo.completed == true ? "completed" : "Not completed"}</p>
+            <h4>${todo.title}</h4>
+        </div>
+        `
+        todoContainer.append(todoCard)
+    });
+    
+})
+
+
+loadTodo();
