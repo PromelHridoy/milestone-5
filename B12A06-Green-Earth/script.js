c4aafabb349144bb436504dc547ae0667b1f6da3
PromelHridoy <promel.hridoy12@gmail.com>
2026-03-05 09:22:56 +0600

    project creation is complete.

---
+++ B12A06-Green-Earth/script.js
@@ -0,0 +1,17 @@
+
+
+async function loadCategories () {
+fetch("https://openapi.programming-hero.com/api/categories")
+.then((res) => res.json())
+.then((data) => {
+    console.log(data);
+    
+})
+.catch((e) => console.log(e));
+
+const res = await fetch("https://openapi.programming-hero.com/api/categories");
+console.log(res);
+
+}
+
+loadCategories();
--- english-janala/script/index.js
+++ english-janala/script/index.js
@@ -5,6 +5,12 @@ const createElements = (arr) => {
     
 }
 
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
+
 const manageSpinner = (status) => {
     if(status == true) {
         document.getElementById("spinner").classList.remove("hidden");
@@ -112,7 +118,7 @@ const displayLevelWord = (words) => {
             <div class="font-bangla text-2xl font-medium">${word.meaning ? word.meaning : "meaning paoya jayni"} / ${word.pronunciation ? word.pronunciation : "pronunciation paoyajayni"}</div>
             <div class="flex justify-between items-center">
                 <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
-                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+                <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
         </div>
         `;
@@ -149,3 +155,23 @@ const displayLesson = (lessons) => {
 }
 
 loadLessons();
+
+document.getElementById("btn-search").addEventListener("click", () => {
+    removeActive();
+
+    const input = document.getElementById("input-search");
+    const searchValue = input.value.trim().toLowerCase();
+    console.log(searchValue);
+
+    fetch("https://openapi.programming-hero.com/api/words/all")
+    .then((res) => res.join())
+    .then((data) => {
+        const allWords = data.data;
+        console.log(allWords);
+        
+        const filterWords = allWords.filter(word => word.word.toLowerCase().includes(searchValue));
+        // console.log(filterWords);
+
+        displayLevelWord(filterWords);
+    })
+})
